15a3d7cf02d2e939175efbf9645c2b7ff0aea242
limjewon <jewon945@gmail.com>
2014-07-31 16:52:43 +0900

    수정

--- Twinkle/src/hanium/twinkle/MainActivity.java
+++ Twinkle/src/hanium/twinkle/MainActivity.java
@@ -1,61 +1,46 @@
 package hanium.twinkle;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements OnClickListener{
 
+	Button btn_light;
+	Button btn_prox;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
+		btn_light = (Button)findViewById(R.id.button1);
+		btn_light.setOnClickListener(this);
+		
+		btn_prox = (Button)findViewById(R.id.button2);
+		btn_prox.setOnClickListener(this);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onClick(View v){
+		int id = v.getId();
+		switch(id){
+		case R.id.button1:
+			Intent intent = new Intent(this,Light_Sensor.class);
+			startActivity(intent);
+			finish();
+			break;
+		case R.id.button2:
+			Intent intent2 = new Intent(this,Proximity_Sensor.class);
+			startActivity(intent2);
+			finish();
+			break;
+		}
+		
+		
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
 }
